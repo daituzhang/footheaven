f80dbf7ce34cfed9657ca0440b09aa7a1e2a1328
yulu zhang <yulu@70kft.com>
2015-12-04 16:57:38 -0600

    finshed a lot

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var scroll = {
+  'cs' : 0,
+  'csb' : $(window).height()
+};
+
 function events() {
   var events = $('.events-row');
   var total = events.length;
@@ -53,11 +58,83 @@ function events() {
       }
     }
   });
+}
 
+function bookOpen(){
+  $('.open-trigger').click(function (e) {
+    e.preventDefault();
+    $('.book:not(.open)').addClass('open hover');
+    $('#example:not(.open)').addClass('open');
+    $('body:not(.lock)').addClass('lock');
+  });
+    $('.close-trigger').click(function (e) {
+    e.preventDefault();
+    $('.book.open').removeClass('open');
+    $('#example.open').removeClass('open');
 
+    setTimeout(function(){
+      $('.book.hover').removeClass('hover');
+      $('body.lock').removeClass('lock');
+    }, 2000);
+  });
+}
+
+function navRoll(){
+  function setNav(){
+    var $currentSection=$('#the-book');
+    if(scroll.cs > 0){
+      $('#site-header:not(.short)').addClass('short');
+    }
+    else {
+      $('#site-header.short').removeClass('short');
+    }
+    if($('body').hasClass('home')){
+      $('section').each(function(){
+        var sectionTop = $(this).offset().top;
+        if( sectionTop - 45 < scroll.cs){
+          // We have either read the section or are currently reading the section so we'll call it our current section
+          $currentSection = $(this);
+          console.log('what');
+          // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
+        }
+        // This is the bit of code that uses the currentSection as its source of ID
+        var id = $currentSection.attr('id');
+        $('#site-nav').find('a').removeClass('active');
+        $('#site-nav').find('[href=#'+id+']').addClass('active');
+      });
+    }
+  }
+  setNav();
+  $(window).scroll( function () {
+    scroll.cs = $(this).scrollTop();
+    setNav();
+  });
+}
+
+function anchorScroll() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 }
 
 $(document).ready(function() {
-  console.log('hsdfasfello');
   events();
+  bookOpen();
+  navRoll();
+  anchorScroll();
+});
+
+$(window).load(function() {
+  setTimeout(function(){
+    $('body:not(.loaded)').addClass('loaded');
+  }, 1000);
 });
