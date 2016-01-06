92db6022f98fd3da6c1a65a029c169d388f4c66a
yulu zhang <yulu@70kft.com>
2016-01-06 16:42:45 -0600

    back general

--- craft/config/db.php
+++ craft/config/db.php
@@ -1,55 +1,55 @@
-// <?php
+<?php
 
-// return array(
-//     '*' => array(
-//         'tablePrefix' => 'craft',
-//     ),
-//     'dev' => array(
-//         'server' => 'localhost',
-//         'user' => 'root',
-//         'password' => '',
-//         'database' => 'janet',
-//     ),
-//     // Treat *.vagrantshare.com the same as localhost
-//     'vagrantshare' => array(
-//         'server' => 'localhost',
-//         'user' => 'root',
-//         'password' => '',
-//         'database' => 'mpv',
-//     ),
+return array(
+    '*' => array(
+        'tablePrefix' => 'craft',
+    ),
+    'dev' => array(
+        'server' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'database' => 'janet',
+    ),
+    // Treat *.vagrantshare.com the same as localhost
+    'vagrantshare' => array(
+        'server' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'database' => 'mpv',
+    ),
 
-//     'yulu-zhang-macbook-pro.local' => array(
-//         'server' => 'localhost',
-//         'user' => 'root',
-//         'password' => '',
-//         'database' => 'janet',
-//     ),
+    'yulu-zhang-macbook-pro.local' => array(
+        'server' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'database' => 'janet',
+    ),
 
-//     '10.0.1.87' => array(
-//         'server' => 'localhost',
-//         'user' => 'root',
-//         'password' => '',
-//         'database' => 'janet',
-//     ),
+    '10.0.1.87' => array(
+        'server' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'database' => 'janet',
+    ),
 
-//     'janet.70kft.com' => array(
-//         'server' => 'localhost',
-//         'user' => 'janet',
-//         'password' => 'Secure2015',
-//         'database' => 'janet',
-//     ),
+    'janet.70kft.com' => array(
+        'server' => 'localhost',
+        'user' => 'janet',
+        'password' => 'Secure2015',
+        'database' => 'janet',
+    ),
     
-//     // 'findinggodeveryday.com' => array(
-//     //     'server' => 'localhost',
-//     //     'user' => 'root',
-//     //     'password' => 'CRgL4TnYvZbR9xGh',
-//     //     'database' => 'janet',
-//     // ),
-//     'lysm-zjrh.accessdomain.com' => array(
-//         'server' => 'localhost',
-//         'user' => 'root',
-//         'password' => 'CRgL4TnYvZbR9xGh',
-//         'database' => 'janet',
-//     ),
+    // 'findinggodeveryday.com' => array(
+    //     'server' => 'localhost',
+    //     'user' => 'root',
+    //     'password' => 'CRgL4TnYvZbR9xGh',
+    //     'database' => 'janet',
+    // ),
+    'lysm-zjrh.accessdomain.com' => array(
+        'server' => 'localhost',
+        'user' => 'root',
+        'password' => 'CRgL4TnYvZbR9xGh',
+        'database' => 'janet',
+    ),
   
-// );
+);
--- craft/config/general.php
+++ craft/config/general.php
@@ -1,82 +1,82 @@
-// <?php
+<?php
 
-// return array(
-//     '*' => array(
-//         'omitScriptNameInUrls' => true,
-//         'maxUploadFileSize' => 335544322,
-//         'siteFullUrl' => 'http://findinggodeveryday.com'
-//     ),
+return array(
+    '*' => array(
+        'omitScriptNameInUrls' => true,
+        'maxUploadFileSize' => 335544322,
+        'siteFullUrl' => 'http://findinggodeveryday.com'
+    ),
 
-//     'dev' => array(
-//         'devMode' => true,
-//         'environmentVariables' => array(
-//             'env' => 'dev',
-//             'fileSystemPath' => '/vagrant/app/',
-//             'frontEndAssets' => '/src/',
-//             'siteName' => 'Finding God Everyday',
-//         )
-//     ),
-//     'localhost' => array(
-//         'devMode' => true,
-//         'environmentVariables' => array(
-//             'env' => 'dev',
-//             'fileSystemPath' => '/vagrant/app/',
-//             'frontEndAssets' => '/src/',
-//             'siteName' => 'Finding God Everyday',
-//         )
-//     ),
+    'dev' => array(
+        'devMode' => true,
+        'environmentVariables' => array(
+            'env' => 'dev',
+            'fileSystemPath' => '/vagrant/app/',
+            'frontEndAssets' => '/src/',
+            'siteName' => 'Finding God Everyday',
+        )
+    ),
+    'localhost' => array(
+        'devMode' => true,
+        'environmentVariables' => array(
+            'env' => 'dev',
+            'fileSystemPath' => '/vagrant/app/',
+            'frontEndAssets' => '/src/',
+            'siteName' => 'Finding God Everyday',
+        )
+    ),
 
-//     '10.0.1.87' => array(
-//         'devMode' => true,
-//         'environmentVariables' => array(
-//             'env' => 'dev',
-//             'fileSystemPath' => '/vagrant/app/',
-//             'frontEndAssets' => '/src/',
-//             'siteName' => 'Finding God Everyday',
-//         )
-//     ),
+    '10.0.1.87' => array(
+        'devMode' => true,
+        'environmentVariables' => array(
+            'env' => 'dev',
+            'fileSystemPath' => '/vagrant/app/',
+            'frontEndAssets' => '/src/',
+            'siteName' => 'Finding God Everyday',
+        )
+    ),
 
-//     'yulu-zhang-macbook-pro.local' => array(
-//         'devMode' => true,
-//         'environmentVariables' => array(
-//             'env' => 'dev',
-//             'fileSystemPath' => '/vagrant/app/',
-//             'frontEndAssets' => '/src/',
-//             'siteName' => 'Finding God Everyday',
-//         )
-//     ),
+    'yulu-zhang-macbook-pro.local' => array(
+        'devMode' => true,
+        'environmentVariables' => array(
+            'env' => 'dev',
+            'fileSystemPath' => '/vagrant/app/',
+            'frontEndAssets' => '/src/',
+            'siteName' => 'Finding God Everyday',
+        )
+    ),
 
-//     'janet.70kft.com' => array(
-//         'devMode' => true,
-//         'environmentVariables' => array(
-//             'env' => 'dev',
-//             'siteUrl'        => '/',
-//             'fileSystemPath' => '/var/www/vhosts/stratospherecreative.com/subdomains/janet/app/',
-//             'frontEndAssets' => '/src/',
-//             'siteName' => 'Finding God Everyday',
-//         )
-//     ),
+    'janet.70kft.com' => array(
+        'devMode' => true,
+        'environmentVariables' => array(
+            'env' => 'dev',
+            'siteUrl'        => '/',
+            'fileSystemPath' => '/var/www/vhosts/stratospherecreative.com/subdomains/janet/app/',
+            'frontEndAssets' => '/src/',
+            'siteName' => 'Finding God Everyday',
+        )
+    ),
 
-//     'lysm-zjrh.accessdomain.com' => array(
-//         'devMode' => true,
-//         'environmentVariables' => array(
-//             'env' => 'dev',
-//             'siteUrl'        => '/',
-//             'fileSystemPath' => '/var/www/findinggodeveryday.com/public/',
-//             'frontEndAssets' => '/src/',
-//             'siteName' => 'Finding God Everyday',
-//         )
-//     ),
-//     // Treat *.vagrantshare.com the same as localhost
-//     'vagrantshare' => array(
-//         'devMode' => true,
-//         'environmentVariables' => array(
-//             'env' => 'dev',
-//             'fileSystemPath' => '/vagrant/public/',
-//             'frontEndAssets' => '/src/',
-//             'siteName' => 'My Personal Valet',
-//         )
-//     ),
+    'lysm-zjrh.accessdomain.com' => array(
+        'devMode' => true,
+        'environmentVariables' => array(
+            'env' => 'dev',
+            'siteUrl'        => '/',
+            'fileSystemPath' => '/var/www/findinggodeveryday.com/public/',
+            'frontEndAssets' => '/src/',
+            'siteName' => 'Finding God Everyday',
+        )
+    ),
+    // Treat *.vagrantshare.com the same as localhost
+    'vagrantshare' => array(
+        'devMode' => true,
+        'environmentVariables' => array(
+            'env' => 'dev',
+            'fileSystemPath' => '/vagrant/public/',
+            'frontEndAssets' => '/src/',
+            'siteName' => 'My Personal Valet',
+        )
+    ),
 
 
-// );
+);
