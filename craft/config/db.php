fa57bc8dd0228220c25a9e683e2db969ef1677e8
yulu zhang <yulu@70kft.com>
2016-04-28 21:49:46 -0500

    add domain

--- craft/config/db.php
+++ craft/config/db.php
@@ -16,46 +16,11 @@ return array(
         'password' => 'footheaven1976',
         'database' => 'footheaven',
     ),
-    // Treat *.vagrantshare.com the same as localhost
-    'vagrantshare' => array(
+    'footheavenrelax.com' => array(
         'server' => 'localhost',
-        'user' => 'root',
-        'password' => '',
-        'database' => 'mpv',
-    ),
-
-    'yulu-zhang-macbook-pro.local' => array(
-        'server' => 'localhost',
-        'user' => 'root',
-        'password' => '',
-        'database' => 'janet',
-    ),
-
-    '10.0.1.87' => array(
-        'server' => 'localhost',
-        'user' => 'root',
-        'password' => '',
-        'database' => 'janet',
-    ),
-
-    'janet.70kft.com' => array(
-        'server' => 'localhost',
-        'user' => 'janet',
-        'password' => 'Secure2015',
-        'database' => 'janet',
-    ),
-    
-    // 'findinggodeveryday.com' => array(
-    //     'server' => 'localhost',
-    //     'user' => 'root',
-    //     'password' => 'CRgL4TnYvZbR9xGh',
-    //     'database' => 'janet',
-    // ),
-    'lysm-zjrh.accessdomain.com' => array(
-        'server' => 'localhost',
-        'user' => 'root',
-        'password' => 'CRgL4TnYvZbR9xGh',
-        'database' => 'janet',
-    ),
+        'user' => 'footheaven',
+        'password' => 'footheaven1976',
+        'database' => 'footheaven',
+    )
   
 );
--- craft/config/general.php
+++ craft/config/general.php
@@ -4,7 +4,7 @@ return array(
     '*' => array(
         'omitScriptNameInUrls' => true,
         'maxUploadFileSize' => 335544322,
-        'siteFullUrl' => 'http://findinggodeveryday.com'
+        'siteFullUrl' => 'http://footheavenrelax.com'
     ),
 
     'localhost' => array(
@@ -27,47 +27,17 @@ return array(
         )
     ),
 
-    'yulu-zhang-macbook-pro.local' => array(
-        'devMode' => true,
-        'environmentVariables' => array(
-            'env' => 'dev',
-            'fileSystemPath' => '/vagrant/app/',
-            'frontEndAssets' => '/src/',
-            'siteName' => 'Finding God Everyday',
-        )
-    ),
 
-    'janet.70kft.com' => array(
+    'footheavenrelax.com' => array(
         'devMode' => true,
         'environmentVariables' => array(
             'env' => 'dev',
-            'siteUrl'        => '/',
-            'fileSystemPath' => '/var/www/vhosts/stratospherecreative.com/subdomains/janet/app/',
+            'fileSystemPath' => '/var/www/app',
             'frontEndAssets' => '/src/',
-            'siteName' => 'Finding God Everyday',
+            'siteName' => 'Foot Heaven',
         )
-    ),
+    )
 
-    'lysm-zjrh.accessdomain.com' => array(
-        'devMode' => true,
-        'environmentVariables' => array(
-            'env' => 'dev',
-            'siteUrl'        => '/',
-            'fileSystemPath' => '/var/www/findinggodeveryday.com/public/',
-            'frontEndAssets' => '/src/',
-            'siteName' => 'Finding God Everyday',
-        )
-    ),
-    // Treat *.vagrantshare.com the same as localhost
-    'vagrantshare' => array(
-        'devMode' => true,
-        'environmentVariables' => array(
-            'env' => 'dev',
-            'fileSystemPath' => '/vagrant/public/',
-            'frontEndAssets' => '/src/',
-            'siteName' => 'My Personal Valet',
-        )
-    ),
 
 
 );
